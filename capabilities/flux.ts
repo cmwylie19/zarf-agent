80201d7e10ff74f40bf8622e3102c3d861c8ad7b
Case Wylie <cmwylie19@defenseunicorns.com>
2023-06-15 08:50:32 -0400

    [TASK] demo capabilities
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

---
+++ capabilities/flux.ts
@@ -0,0 +1,18 @@
+//import Pods from "lib/hooks/pods/pods";
+// const flux;
+
+// When(a.Flux)
+    // .isCreatedOrUpdated()
+    // .Then(async request => {
+    //     pod = new Pod(request)
+    //     if (pod.hasIgnoreLabels()) {
+    //         // bail
+    //         return
+    //     } else {
+    //         await pod.deployImagePullSecret()
+    //         await pod.replaceImage()
+    //         await pod.addImagePullSecret()
+    //         await pod.addUpdatedAnnotation()
+
+    //     }
+    // })
